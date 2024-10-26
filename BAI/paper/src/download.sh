b4c74dac1597b23f0f3000680044d17a62bd9b2b
Leopold Lemmermann <leopold@lemmermann.xyz>
2024-10-26 14:03:10 +0000

    updated bai paper download script.

--- BAI/paper/src/download.sh
+++ BAI/paper/src/download.sh
@@ -1,7 +1,9 @@
 DEST=${1:-/workspaces/w24/BAI/paper/src/data/flickr8k}
 
-# Download flickr8k dataset
-mkdir -p $DEST
+# Download flickr8k dataset#
+if [ ! -d $DEST ]; then
+  mkdir -p $DEST
+fi
 curl -L -o $DEST/flickr8k.zip https://www.kaggle.com/api/v1/datasets/download/adityajn105/flickr8k
 unzip $DEST/flickr8k.zip -d $DEST
 rm $DEST/flickr8k.zip
